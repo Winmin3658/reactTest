730dbfd80d245f0510977e248b1eb46d9a5f2825
Winmin3658 <dltmdals4101321@naver.com>
2025-07-09 01:14:46 +0900

    [update] test update, remove 기능 추가

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
   const [phone, setPhone] = useState("");
+
   const registUser = () => {
     const user = { name, age, gender, phone };
     setUserList([...userList, user]);
@@ -20,10 +21,22 @@ function App() {
     setGender("");
     setPhone("");
   };
+
+  const deleteUser = (index) => {
+    const newList = userList.filter((_, i) => i !== index);
+    setUserList(newList);
+  };
+
+  const updateUser = (index, updatedUser) => {
+    const newList = [...userList];
+    newList[index] = updatedUser;
+    setUserList(newList);
+  };
+
   return (
     <div className="App">
       <h1>회원 정보 출력</h1>
-      <hr></hr>
+      <hr />
       <table className="member_tbl">
         <thead>
           <tr>
@@ -31,17 +44,25 @@ function App() {
             <th>나이</th>
             <th>성별</th>
             <th>전화번호</th>
+            <th>관리</th>
           </tr>
         </thead>
         <tbody>
-          {userList.map((item, index) => {
-            return <User key={"user" + index} user={item} />;
-          })}
+          {userList.map((user, index) => (
+            <User
+              key={index}
+              user={user}
+              index={index}
+              deleteUser={deleteUser}
+              updateUser={updateUser}
+            />
+          ))}
         </tbody>
       </table>
+
       <div className="regist-wrap">
         <h3>회원 정보 등록</h3>
-        <hr></hr>
+        <hr />
         <InputWrap text="이름" data={name} setData={setName} />
         <InputWrap text="나이" data={age} setData={setAge} />
         <InputWrap text="성별" data={gender} setData={setGender} />
@@ -52,29 +73,54 @@ function App() {
   );
 }
 
-const User = (props) => {
-  const user = props.user;
+const User = ({ user, index, deleteUser, updateUser }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editUser, setEditUser] = useState({ ...user });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSave = () => {
+    updateUser(index, editUser);
+    setIsEdit(false);
+  };
+
   return (
     <tr>
-      <td>{user.name}</td>
-      <td>{user.age}</td>
-      <td>{user.gender}</td>
-      <td>{user.phone}</td>
+      {isEdit ? (
+        <>
+          <td><input name="name" value={editUser.name} onChange={handleChange} /></td>
+          <td><input name="age" value={editUser.age} onChange={handleChange} /></td>
+          <td><input name="gender" value={editUser.gender} onChange={handleChange} /></td>
+          <td><input name="phone" value={editUser.phone} onChange={handleChange} /></td>
+          <td>
+            <button onClick={handleSave}>저장</button>
+            <button onClick={() => setIsEdit(false)}>취소</button>
+          </td>
+        </>
+      ) : (
+        <>
+          <td>{user.name}</td>
+          <td>{user.age}</td>
+          <td>{user.gender}</td>
+          <td>{user.phone}</td>
+          <td>
+            <button onClick={() => setIsEdit(true)}>수정</button>
+            <button onClick={() => deleteUser(index)}>삭제</button>
+          </td>
+        </>
+      )}
     </tr>
   );
 };
 
-const InputWrap = (props) => {
-  const text = props.text;
-  const data = props.data;
-  const setData = props.setData;
-  const changeInputValue = (e) => {
-    setData(e.target.value);
-  };
+const InputWrap = ({ text, data, setData }) => {
   return (
     <div className="input_wrap">
       <label>{text}</label>
-      <input type="text" value={data} onChange={changeInputValue} />
+      <input type="text" value={data} onChange={(e) => setData(e.target.value)} />
     </div>
   );
 };
